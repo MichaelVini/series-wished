9f18706f6c5cdd37cb85d744d9eb7f6dc29fb0b9
Michael Vinicius <michaelvini62@gmail.com>
2021-10-23 22:07:57 -0300

    import and use VueResource

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 import router from './router'
 import store from './store'
+import VueResource from 'vue-resource'
+import './assets/scss/app.scss'
 
 Vue.config.productionTip = false
 
+Vue.use(VueResource)
+
 new Vue({
   router,
+  VueResource,
   store,
   render: h => h(App)
 }).$mount('#app')
