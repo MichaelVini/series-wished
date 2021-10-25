b58570ba8d53a521857da906e656e21c0258c445
Michael Vinicius <michaelvini62@gmail.com>
2021-10-24 21:26:29 -0300

    export store auth module

---
+++ src/modules/auth/store/index.js
@@ -0,0 +1,12 @@
+import state from './state'
+import * as actions from './actions'
+import mutations from './mutations'
+import * as types from './mutations-types'
+
+export default {
+  state,
+  actions,
+  mutations,
+  types,
+  namespaced: true
+}
